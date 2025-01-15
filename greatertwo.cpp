f8c99e009832ae8843c34bdd1300803895663d98
Priyanshu <kharbashpriyanshu@gmail.com>
2025-01-15 08:40:35 -0500

    Add files via upload

---
+++ greatertwo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std; 
+int main() {
+   int a;
+   int b;
+   cout<<"a=";
+   cin>>a;
+   cout<<"b=";
+   cin>>b;
+   if(a>b){
+       cout<<a;
+   }
+   else{
+       cout<<b;
+   }
+}
