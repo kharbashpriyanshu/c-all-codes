93fdbaa056d661ed751a403a81cea5fdc37b3f9a
Priyanshu <kharbashpriyanshu@gmail.com>
2025-01-21 16:31:36 +0530

    Add files via upload

---
+++ use::.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+class employee{
+char name[20];
+int age;
+public:
+void inputdetail();
+void outputdetail();
+};
+void employee::inputdetail() {
+//cout<<"name of employee : ";
+cin>>name;
+//cout<<"age of employee : ";
+cin>>age;
+}
+void employee::outputdetail() {
+cout<<"name of employee : "<<name;
+cout<<"\nage of employee : "<<age;
+}
+int main(){
+employee e1;
+e1.inputdetail();
+e1.outputdetail();
+}
